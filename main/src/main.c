d0d3e3eacd6d917187cf3a16fb399f1932738f8b
Scriptor25 <f.schreiber.2006@gmail.com>
2026-05-03 11:06:28 +0200

    switched song, updated main cmake list

--- main/src/main.c
+++ main/src/main.c
@@ -1,5 +1,5 @@
 #include "engine.h"
-#include "songs/megalovania.h"
+#include "songs/golden.h"
 
 #include <stdio.h>
 
@@ -169,8 +169,7 @@ void app_main(void) {
 
       ENVELOPES, sizeof(ENVELOPES) / sizeof(envelope_data_t),
 
-      megalovania_data,
-      sizeof(megalovania_data) / sizeof(track_data_t),
+      golden_data, sizeof(golden_data) / sizeof(track_data_t),
 
       PINS, sizeof(PINS) / sizeof(int)
 
