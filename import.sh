0730957293d6228b3c7b3d06355c2a239e384e98
Scriptor25 <f.schreiber.2006@gmail.com>
2026-05-05 10:47:36 +0200

    moved mid files into project, extended playlist

--- import.sh
+++ import.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-names="all_night_long axelf fade free_bird golden hotel_california mayday megalovania mission_impossible tetris the_real_slim_shady what_ive_done"
+names="all_night_long aria_math axelf bohemian_rhapsody dont_look_back_in_anger fade final_countdown free_bird golden highway_to_hell hotel_california its_my_life jump mayday megalovania mission_impossible never_gonna_give_you_up sharp_dressed_man stairway_to_heaven still_alive super_mario_bros sweden take_on_me tetris the_real_slim_shady through_the_fire_and_flames toccata_and_fugue what_ive_done where_is_my_mind"
 
 for name in $names; do
     echo "processing $name..."
-    python "scripts/midi.py" "/home/felix/Music/$name.mid" "$name" > "main/include/songs/$name.h"
+    python "scripts/midi.py" "songs/$name.mid" "$name" > "main/include/songs/$name.h"
 done
